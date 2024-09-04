01a826f7200e4f8d1dea6b3f40a8fb3481098d2c
M4theusVieir4 <https://github.com/M4theusVieir4>
2024-09-04 18:58:33 -0300

    Adicionando Camada de Services e Interfaces para produto e serviços

--- CleanArchMvc.Application/obj/Debug/net8.0/CleanArchMvc.Application.AssemblyInfo.cs
+++ CleanArchMvc.Application/obj/Debug/net8.0/CleanArchMvc.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanArchMvc.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc2c68c2a0c80cd64609f019e1aaea746a250fb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cdc54740d562705982d4af298ff9ffcaca7d735")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanArchMvc.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanArchMvc.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
